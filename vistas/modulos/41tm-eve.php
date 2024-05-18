deecd24ee87ca22f4e1335d3edd171e1e5b4a358
glapettina <guillermo@glsoftware.com.ar>
2024-05-18 16:23:20 -0300

    Espacio EVE Terminado

---
+++ vistas/modulos/41tm-eve.php
@@ -0,0 +1,367 @@
+<?php
+  
+
+    $item = null;
+    $valor = null;   
+
+    $cursos = ControladorCursos::ctrMostrarCursos($item, $valor);
+
+
+?>       
+
+
+  <div class="content-wrapper">
+    
+    <section class="content-header">
+      <h1>
+        4º Primera TM - Espacio E.V.E.
+        
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="inicio"><i class="fa fa-dashboard"></i> Inicio</a></li>
+        <li class="active">4º Primera TM - Espacio E.V.E.</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+      <!-- Default box -->
+      <div class="box">
+
+        <?php
+
+          if ($_SESSION["perfil"] != "Preceptor") {
+            
+              echo '<div class="box-header with-border">
+                        
+                <button type="submit" class="btn btn-danger" idCurso=13 tabla="cuarto" data-toggle="modal" data-target="#modalCopiaSaberes">
+                  
+                  Copia Saberes
+                </button>
+
+                <button class="btn btn-primary btnInformeArea" area="eve" periodo="'.$_SESSION['periodo'].'" idCurso=13 tabla="cuarto" informe="informe-area">
+                  
+                  Informes Curso
+                </button>
+                
+              </div>';
+
+          }
+
+        ?>
+
+
+
+        <div class="box-body">
+          
+          <table class="table table-bordered table-striped dt-responsive tablas">
+            
+            <thead>
+              
+              <tr>
+                
+                <th style="width: 10px">#</th>
+                <th>DNI</th>
+                <th>Apellidos y Nombres</th>
+                <th>Curso</th>
+                <th>Turno</th>
+                <th>Acciones</th>
+
+              </tr> 
+
+            </thead>
+            <tbody>
+
+              <?php
+
+                  $item = "id_curso";
+                  $valor = 13;
+                  $tabla = "cuarto";
+                  $periodo = $_SESSION["periodo"];
+                  $verifica = true;
+
+
+                  $informes = ControladorInformes::ctrMostrarInformes($item, $valor, $tabla, $periodo, $verifica);
+
+
+                  foreach ($informes as $key => $value) {
+                    
+                    echo '<tr>
+                
+                          <td>'.($key+1).'</td>
+                          <td>'.$value["documento"].'</td>
+                          <td>'.$value["nombre"].'</td>';
+
+                          $item = "id";
+                          $valor = $value["id_curso"];
+
+                          $curso = ControladorCursos::ctrMostrarCursos($item, $valor);
+
+                          echo '<td>'.$curso["nombre"].'</td>  
+
+                          <td>'.$curso["turno"].'</td>';
+
+
+                          echo '<td>';
+
+
+                          if ($_SESSION["perfil"] != "Preceptor") {
+
+                            
+                            echo'<div class="btn-group">
+
+                              <button class="btn btn-warning btnEditarInformeEve" nombreAlumno="'.$value["nombre"].'" tabla="cuarto" periodo="'.$_SESSION['periodo'].'" idAlumno="'.$value["id"].'" data-toggle="modal" data-target="#modalEditarInforme"><i class="fa fa-pencil"></i></button>
+                              
+                            </div>';
+                            
+                             
+                            }
+
+
+                            echo '<div class="btn-group">
+                                
+                              <button class="btn btn-primary btnImprimirInformeIndividual" informe="informe-individual" periodo="'.$_SESSION['periodo'].'" tabla="cuarto" area="eve" idAlumno="'.$value["id"].'" data-toggle="modal" data-target="#modalImprimirInformeIndividual"><i class="fa fa-print"></i></button>
+                              
+                            </div>
+
+                          </td>
+
+                        </tr> ';
+                  }
+
+
+              ?>
+
+                           
+
+            </tbody>
+
+          </table>
+
+        </div>
+        
+        
+      </div>
+
+    </section>
+  </div>
+
+
+  
+
+
+<!--=====================================
+  MODAL EDITAR INFORME
+  ======================================-->
+
+
+<div id="modalEditarInforme" class="modal fade" role="dialog">
+
+  <div class="modal-dialog">
+
+    <div class="modal-content">
+
+      <form role="form" method="post" id="modalInforme">
+
+          <!--=====================================
+          CABEZA DEL MODAL
+          ======================================-->
+
+          <div class="modal-header" style="background: #3c8dbc; color: white">
+
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+
+           
+            <h4 class="modal-title" id="alumnoEdicion"></h4>
+
+          </div>
+
+          <!--=====================================
+          CUERPO DEL MODAL
+          ======================================-->
+
+          <div class="modal-body">
+
+            <div class="box-body">
+
+              <!-- ENTRADA PARA LOS SABERES -->          
+              
+              <div class="form-group">
+                
+                  <label for="saberesEve">Saberes</label>
+                      <textarea class="form-control" cols="80" rows="6" id="saberesEve" name="saberesEve">
+                  </textarea>
+              </div>
+
+        <!-- ENTRADA PARA LA APRECIACION --> 
+
+              
+              <div class="form-group">
+                
+                <div class="input-group">
+                  
+                  <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                  
+                  <select class="form-control input-lg" id="apreciaEve" name="apreciaEve">
+
+                  <option value="">Apreciación Cualitativa</option>
+                  <option value="Se Apropió de los Saberes">Se Apropió de los Saberes</option>
+                  <option value="En Proceso de Apropiación de Saberes">En Proceso de Apropiación de Saberes</option>
+                  <option value="No se Apropió de los Saberes">No se Apropió de los Saberes</option>
+                                        
+
+                  </select>
+
+                </div>
+
+              </div>
+
+              <!-- ENTRADA PARA LA ASISTENCIA --> 
+
+              
+              <div class="form-group">
+                
+                <div class="input-group">
+                  
+                  <span class="input-group-addon"><i class="fa fa-th"></i></span>
+                  <input class="form-control input-lg" type="number" name="asistenciaEve" id="asistenciaEve" placeholder="Ingresar asistencia (%)" required>
+
+                </div>
+
+              </div>
+
+
+              <div class="form-group">          
+                  <label for="observaEve">Observaciones</label>
+                      <textarea class="form-control" cols="80" rows="3" id="observaEve" name="observaEve">
+                  </textarea>
+              </div>
+
+
+            </div>
+            
+          </div>
+          
+          <!--=====================================
+          PIE DEL MODAL
+          ======================================-->
+
+
+
+          <div class="modal-footer">
+
+            <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
+
+            <button type="submit" class="btn btn-primary">Guardar Cambios</button>
+
+              <input type="hidden" name="idAlumno" id="idAlumno"> 
+
+          </div>       
+
+         <?php
+
+              $tabla = "cuarto";
+              $curso = "41tm-eve";
+
+              $editarInforme = new ControladorInformes();
+              $editarInforme -> ctrEditarInformeEve($tabla, $curso);
+
+          ?>
+
+    </form>
+
+          
+    </div>
+
+  </div>
+</div>
+
+
+<!--=====================================
+  MODAL COPIA SABERES
+  ======================================-->
+
+
+<div id="modalCopiaSaberes" class="modal fade" role="dialog">
+
+  <div class="modal-dialog">
+
+    <div class="modal-content">
+
+      <form role="form" method="post">
+
+          <!--=====================================
+          CABEZA DEL MODAL
+          ======================================-->
+
+          <div class="modal-header" style="background: #3c8dbc; color: white">
+
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+
+           
+              <h4 class="modal-title">Copia Saberes</h4>
+
+          </div>
+
+          <!--=====================================
+          CUERPO DEL MODAL
+          ======================================-->
+
+          <div class="modal-body">
+
+            <div class="box-body">
+
+              <!-- ENTRADA PARA LOS SABERES -->          
+              
+              <div class="form-group">
+                
+                  <label for="copiaSaberesEve">Saberes</label>
+                      <textarea class="form-control" cols="80" rows="6" id="copiaSaberesEve" name="copiaSaberesEve">
+                  </textarea>
+              </div>
+
+
+            </div>
+            
+          </div>
+          
+          <!--=====================================
+          PIE DEL MODAL
+          ======================================-->
+
+
+
+          <div class="modal-footer">
+
+            <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
+           
+
+            <button type="submit" class="btn btn-primary">Copiar Saberes</button>
+
+              <input type="hidden" name="idAlumno" id="idAlumno"> 
+
+          </div>       
+
+         <?php
+
+              $tabla = "cuarto";
+              $curso = "41tm-eve";
+              $ncurso = 13;
+              $periodo = $_SESSION["periodo"];
+
+              $copiaSaberes = new ControladorInformes();
+              $copiaSaberes -> ctrCopiarSaberesEve($tabla, $curso, $ncurso, $periodo);
+
+          ?>
+
+    </form>
+
+          
+    </div>
+
+  </div>
+</div>
+
+
+
